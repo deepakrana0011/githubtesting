5169744979955b66893e885e2523086a7eed95f9
webexpert4rv <webexpert4rv@gmail.com>
2019-11-27 14:16:00 +0530

    vk 27-11-19 01-00pm

--- app/src/main/java/com/example/testingpurpose/MainActivity.kt
+++ app/src/main/java/com/example/testingpurpose/MainActivity.kt
@@ -9,8 +9,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.e("wow","this is for testing purpose");
-        Log.e("wow","this is for testing purpodfdfsse");
-        Log.e("wow3","this is for testing purpose");
+        Log.e("wow","this is for testing purpose")
+        Log.e("wow","this is for testing purpodfdfsse")
+        Log.e("wow3","this is for testing purpose")
     }
 }
